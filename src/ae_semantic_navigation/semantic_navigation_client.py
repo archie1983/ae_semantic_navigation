ff667a4979ea150cab6fb6142388f9bc78326add
Arturs Elksnis <archie1983@hotmail.co.uk>
2026-06-15 13:57:25 +0100

    Item extractor comms work

---
+++ src/ae_semantic_navigation/semantic_navigation_client.py
@@ -0,0 +1,146 @@
+import zmq
+import numpy as np
+import time, cv2, os
+from PIL import Image
+
+class SemanticNavigationClient:
+	def __init__(self, jetson_ip, port=5555):
+		self.context = zmq.Context()
+		self.socket = self.context.socket(zmq.REQ)  # REQuest socket
+		self.socket.connect(f"tcp://{jetson_ip}:{port}")
+		print(f"Connected to Jetson at {jetson_ip}:{port}")
+
+	def store_ref_path(self, path_imgs, path_id="?"):
+		"""
+		Send an a collection of images, representing a reference path, to server.
+
+		Args:
+			image_np: numpy array (x, H, W, C) in BGR order (typical from OpenCV/AI2-THOR)
+
+		Returns:
+			success flag or None if error
+		"""
+		# Serialize the images
+		data = {
+			'shape': path_imgs.shape,
+			'dtype': str(path_imgs.dtype),
+			'bytes': path_imgs.tobytes(),
+			'path_id': path_id,
+			'action': "store_ref_path",
+			'module': "path_compare"
+		}
+
+		## debug
+		path_id = str(path_id)
+		os.makedirs(path_id, exist_ok=True)
+		cnt = 0
+		for img in path_imgs:
+			cnt += 1
+			cv2.imwrite(os.path.join(path_id, str(cnt) + ".png"), img)
+		## /debug
+
+		# Send request
+		self.socket.send_pyobj(data)
+
+		# Wait for response (this BLOCKS until Jetson replies)
+		try:
+			response = self.socket.recv_pyobj()
+			return response
+		except zmq.ZMQError as e:
+			print(f"Error receiving response: {e}")
+			return None
+
+	def qry_path_similarity(self, path_imgs):
+		"""
+		Main navigation loop with real-time confidence feedback.
+
+		Args:
+			get_image_func: Function that captures current FPV image from AI2-THOR
+			max_steps: Maximum number of steps to take
+		"""
+		data = {
+			'shape': path_imgs.shape,
+			'dtype': str(path_imgs.dtype),
+			'bytes': path_imgs.tobytes(),
+			'action': "qry_path_similarity",
+			'module': "path_compare"
+		}
+
+		## debug
+		path_id = "tmp_cmp"
+		os.makedirs(path_id, exist_ok=True)
+		cnt = 0
+		for img in path_imgs:
+			cnt += 1
+			cv2.imwrite(os.path.join(path_id, str(cnt) + ".png"), img)
+		## /debug
+
+		# Send request
+		self.socket.send_pyobj(data)
+
+		# Wait for response (this BLOCKS until Jetson replies)
+		try:
+			response = self.socket.recv_pyobj()
+			return response
+		except zmq.ZMQError as e:
+			print(f"Error receiving response: {e}")
+			return None
+
+		# Small delay to avoid overwhelming the system
+		time.sleep(0.05)
+
+	def detect_objects_in_image(self, img):
+		"""
+		Send an a collection of images, representing a reference path, to server.
+
+		Args:
+			image_np: numpy array (x, H, W, C) in BGR order (typical from OpenCV/AI2-THOR)
+
+		Returns:
+			success flag or None if error
+		"""
+		# Serialize the images
+		data = {
+			'shape': img.shape,
+			'dtype': str(img.dtype),
+			'bytes': img.tobytes(),
+			'action': "detect_objects_in_image",
+			'module': "yolo_obj_detector"
+		}
+
+		# Send request
+		self.socket.send_pyobj(data)
+
+		# Wait for response (this BLOCKS until Jetson replies)
+		try:
+			response = self.socket.recv_pyobj()
+			return response
+		except zmq.ZMQError as e:
+			print(f"Error receiving response: {e}")
+			return None
+
+# Example usage
+def gen_n_imgs(img_num):
+	"""Replace with your actual AI2-THOR frame capture"""
+	# Simulate a 64x64 RGB image
+	return np.random.randint(0, 255, (img_num, 64, 64, 3), dtype=np.uint8)
+
+def gen_1_img():
+	"""Replace with your actual AI2-THOR frame capture"""
+	# Simulate a 64x64 RGB image
+	return np.random.randint(0, 255, (64, 64, 3), dtype=np.uint8)
+
+if __name__ == "__main__":
+	# Create agent and connect to Jetson
+	agent = SemanticNavigationClient(jetson_ip="192.168.0.32", port=5555)
+
+	# store reference path images
+	# print(agent.store_ref_path(gen_n_imgs(10), "bathroom"))
+	# print(agent.store_ref_path(gen_n_imgs(10), "kitchen"))
+	# print(agent.store_ref_path(gen_n_imgs(10), "living_room"))
+	# print(agent.qry_path_similarity(gen_n_imgs(3)))
+
+	pil_image = Image.open("/home/hp20024/robotics/latent_planning/dreamerv3/scene_pics/8.png")
+	img_array = np.stack([pil_image], axis = 0)
+	print(agent.detect_objects_in_image(img_array))
+
---
+++ src/ae_semantic_navigation/semantic_navigation_server.py
@@ -0,0 +1,129 @@
+import zmq, os, cv2
+import numpy as np
+from PIL import Image
+from ae_path_compare import PathCompare
+from ultralytics import YOLO
+
+class SemanticNavigationServer:
+	def __init__(self, port=5555, use_dino = True):
+		self.pc = PathCompare(use_dino)
+		self.port = port
+		self.use_dino = use_dino
+
+		# YOLO part
+		# Load the exported TensorRT model
+		#self.item_extractor_model = YOLO("item_extract_ae.engine")
+		self.item_extractor_model = YOLO("item_extract_ae.pt")
+
+		# Run inference
+		#results = trt_model("images/test/h_520_expl_2_120_15.png")
+
+		# Set up ZeroMQ with REP (REPly) socket
+		self.context = zmq.Context()
+		self.socket = self.context.socket(zmq.REP)  # Changed from PULL to REP
+		self.socket.bind(f"tcp://*:{self.port}")
+
+		print(f"Path Compare server running on port {self.port}, waiting for images...")
+		self.path_refs = {}
+
+	# def __init__(self, path_compare_instance, image_port=5555, response_port=5556):
+	# 	self.pc = path_compare_instance
+	# 	self.context = zmq.Context()
+	#
+	# 	# Receive images on PULL socket
+	# 	self.image_socket = self.context.socket(zmq.PULL)
+	# 	self.image_socket.bind(f"tcp://*:{image_port}")
+	#
+	# 	# Send responses on PUSH socket
+	# 	self.response_socket = self.context.socket(zmq.PUSH)
+	# 	self.response_socket.bind(f"tcp://*:{response_port}")
+	#
+	# 	print(f"Server ready: receiving images on {image_port}, sending responses on {response_port}")
+
+	def run(self):
+		while True:
+			# 1. Receive the image data
+			data = self.socket.recv_pyobj()  # This BLOCKS until a request arrives
+
+			if (data['action'] == 'store_ref_path'):
+				# 2. Process the images
+				received_array = np.frombuffer(data['bytes'], dtype=data['dtype'])
+				received_images = received_array.reshape(data['shape'])
+				pil_images = [Image.fromarray(img) for img in received_images]
+				path_id = data['path_id']
+				self.path_refs[path_id] = pil_images
+				# Send the response back
+				response = {
+					'success': True
+				}
+				## debug
+				path_id = str(path_id)
+				os.makedirs(path_id, exist_ok=True)
+				cnt = 0
+				for img in received_images:
+					cnt += 1
+					cv2.imwrite(os.path.join(path_id, str(cnt) + ".png"), img)
+				## /debug
+
+			elif(data['action'] == 'qry_path_similarity'):
+				# 2. Process the images
+				received_array = np.frombuffer(data['bytes'], dtype=data['dtype'])
+				received_images = received_array.reshape(data['shape'])
+				# get x images from the received (x, 64, 64, 3) tensor. This will be our path to compare
+				pil_images = [Image.fromarray(img) for img in received_images]
+				# compare that path against all reference paths
+				if self.use_dino:
+					cmp_res = {k: float(self.pc.compare_paths_mean(v, pil_images)) for k, v in self.path_refs.items()}
+				else:
+					cmp_res = {k: self.pc.fit_cur_path_to_ref_path(v, pil_images)[1] for k, v in self.path_refs.items()}
+
+				#print(cmp_res)
+				# find the best match and return both the score and the reference match buffer
+				best_match = max(cmp_res.items(), key=lambda k: k[1])
+				response = {
+					'best_match_ref': best_match[0],
+					'best_match_score': best_match[1],
+					'success': True
+				}
+				## debug
+				path_id = "tmp_cmp"
+				os.makedirs(path_id, exist_ok=True)
+				cnt = 0
+				for img in received_images:
+					cnt += 1
+					cv2.imwrite(os.path.join(path_id, str(cnt) + ".png"), img)
+				## /debug
+				#for k, v in self.path_refs.items():
+				#	cmp_res = self.pc.compare_paths(v, pil_images)
+			elif (data['action'] == 'detect_objects_in_image'):
+				# 2. Process the images
+				received_array = np.frombuffer(data['bytes'], dtype=data['dtype'])
+				received_img = received_array.reshape(data['shape'])[0]
+				# get x images from the received (x, 64, 64, 3) tensor. This will be our path to compare
+				pil_image = Image.fromarray(received_img)
+
+				item_extractor_res = self.item_extractor_model(pil_image)
+
+				# print("AE Classes: ", item_extractor_res[0].boxes.cls)
+				# print("AE Classes1: ", item_extractor_res[0].names)
+				# print("AE All: ", item_extractor_res[0].boxes)
+
+				item_names = [item_extractor_res[0].names[int(item)] for item in item_extractor_res[0].boxes.cls]
+				#print("item_names: ", item_names)
+
+				response = {
+					'item_names': item_names,
+					'success': True
+				}
+			self.socket.send_pyobj(response)
+
+
+			# # Convert to PIL Image (adjust color channels as needed)
+			# if received_image.shape[2] == 3:
+			# 	pil_image = Image.fromarray(received_image[:, :, ::-1])  # BGR to RGB
+			# else:
+			# 	pil_image = Image.fromarray(received_image)
+
+if __name__ == "__main__":
+	pcs = SemanticNavigationServer()
+	pcs.run()
