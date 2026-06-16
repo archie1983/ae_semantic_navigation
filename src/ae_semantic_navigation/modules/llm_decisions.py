f5705fc8aa366a3ec17d808f99e6b93b06352656
Arturs Elksnis <archie1983@hotmail.co.uk>
2026-06-16 12:08:42 +0100

    bugfi

--- src/ae_semantic_navigation/modules/llm_decisions.py
+++ src/ae_semantic_navigation/modules/llm_decisions.py
@@ -1,10 +1,10 @@
-from ae_llm_navigation_decisions import LLMRoomClassifier, LLMType
+from ae_llm_navigation_decisions import LLMDecisionMaker, LLMType
 import numpy as np
 from PIL import Image
 
 class LLMDecisions:
 	def __init__(self):
-		self.lrc = LLMRoomClassifier(LLMType.MINISTRAL_3_3b_instruct_nf4_bnb)
+		self.ldm = LLMDecisionMaker(LLMType.MINISTRAL_3_3b_instruct_nf4_bnb)
 
 	def classify_room_by_this_object_set_and_pic(self, data):
 		received_array = np.frombuffer(data['bytes'], dtype=data['dtype'])
@@ -14,5 +14,5 @@ class LLMDecisions:
 
 		obj_set = None
 		img_uri = None
-		rt_llm, llm_text = self.lrc.classify_room_by_this_object_set_and_pic(obj_set={"Scales", "bathtub", "toothbrush"}, img_uri=None)
+		rt_llm, llm_text = self.ldm.classify_room_by_this_object_set_and_pic(obj_set={"Scales", "bathtub", "toothbrush"}, img_uri=None)
 		return rt_llm
